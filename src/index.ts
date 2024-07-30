f070d2fa8f1c4fc640806bdf336f453123f0ea8f
davidwrossiter <davewrossiter@gmail.com>
2024-07-30 22:18:47 +0100

    add d1 for storing essays

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,29 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.toml`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import { Hono } from 'hono';
 
-export default {
-	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
-	},
-} satisfies ExportedHandler<Env>;
+export interface Env {
+	DB: D1Database;
+}
+
+const app = new Hono<{ Bindings: Env }>();
+
+app.get('/', async (c) => {
+	return c.text('Hello World! Im DAVID!');
+});
+
+app.get('/essay-list', async (c) => {
+	return c.text('essay list');
+});
+
+app.get('/essay/:id', async (c) => {
+	const data = await c.env.DB.prepare('SELECT * FROM Essays WHERE EssayId = ?').bind(c.req.param('id')).all();
+	// console.log(data);
+
+	const essays = data.results;
+
+	console.log(essays);
+
+	const id: string = c.req.param('id');
+	return c.text(`${JSON.stringify(essays)}` + id);
+});
+
+export default app;
